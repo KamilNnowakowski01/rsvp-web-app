c6271aa2be5d0f854154446babf839ed722b0dc4
Kamil Nowakowski <Nowakooo26x@gmail.com>
2025-08-26 17:16:12 +0200

    setup

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
